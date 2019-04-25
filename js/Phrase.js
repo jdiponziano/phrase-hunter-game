2087205f1f5df1299b184aecbb45576d0a7dadf2
Jessica Di Ponziano <jess@webdevjd.com>
2019-04-25 18:38:49 -0400

    Build addPhraseToDisplay function

--- js/Phrase.js
+++ js/Phrase.js
@@ -3,8 +3,23 @@ class Phrase {
     this.phrase = phrase.toLowerCase();
   }
 
+/**
+ * Creates <li> for each letter and appends it to the page
+ */
   addPhraseToDisplay() {
+    const phraseContainer = document.getElementById('phrase');
+    const characters = this.phrase.split("");
+    const list = '';
+    characters.forEach(function(char) {
+      console.log(char);
+      if (char.match(/^[A-Za-z]+$/)) {
+        list += '<li class="hide letter ' + char + '">' + char + '</li>';
+      } else {
+        list += '<li class="space">' + char + '</li>';
+      }
+    })
 
+    phraseContainer.firstChild.appendChild(list);
   }
 
   checkLetter() {
