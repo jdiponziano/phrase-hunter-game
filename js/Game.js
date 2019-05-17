01044e2267acd0b2244247ea3e7ba028da400eb4
Jessica DiPonziano <jess@webdevjd.com>
2019-05-17 16:26:07 -0400

    Add create phrases function to game object

--- js/Game.js
+++ js/Game.js
@@ -1,9 +1,25 @@
 class Game {
   constructor() {
     this.missed = 0;
-    this.phrases = [];
+    this.phrases = this.createPhrases();
     this.activePhrase = null;
   }
+
+  /**
+  * Creates phrases for use in game
+  * @return {array} An array of phrases that could be used in the game
+  */
+  createPhrases() {
+    const phrases = [];
+
+    phrases.push(new Phrase('Sooner Not Later'));
+    phrases.push(new Phrase('A Dime a Dozen'));
+    phrases.push(new Phrase('Piece of Cake'));
+    phrases.push(new Phrase('An arm and a leg'));
+    phrases.push(new Phrase('Beating around the push'));
+
+    return phrases;
+  };
 
   startGame() {
     const phrase = this.getRandomPhrase;
