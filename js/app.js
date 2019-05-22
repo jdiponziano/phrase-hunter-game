a7376fd95d312779d40415c28d47519241019e91
Jessica DiPonziano <jess@webdevjd.com>
2019-05-22 15:49:17 -0400

    Add keyboard listener to querty keys

--- js/app.js
+++ js/app.js
@@ -4,7 +4,15 @@
 
 const game = new Game();
 const startBtn = document.getElementById('btn__reset');
+const keyboardBtns = document.querySelectorAll('.key');
 
 startBtn.addEventListener('click', () => {
   game.startGame();
+});
+
+keyboardBtns.forEach((key) => {
+  key.addEventListener('click', (evt) => {
+    const letter = evt.target.textContent;
+    game.handleInteraction(letter);
+  })
 });
