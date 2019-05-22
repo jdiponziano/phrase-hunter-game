0351498589be8a8266b646d0f1cb441dcbbc3535
Jessica DiPonziano <jess@webdevjd.com>
2019-05-22 14:56:20 -0400

    Add event listener to button for click to start game

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
+/*
+ * Phrase Hunter Game
  * app.js */
+
+const game = new Game();
+const startBtn = document.getElementById('btn__reset');
+
+startBtn.addEventListener('click', () => {
+  game.startGame();
+});
